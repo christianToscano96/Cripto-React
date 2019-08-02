bb70921683fd334825fa03990e6bcc225acd6255
Christian Toscano <chrismarcadopago@gmail.com>
2019-08-02 01:01:40 -0300

    leyendo los valores del formulario y pasandolos al state

--- src/component/formulario/Formulario.js
+++ src/component/formulario/Formulario.js
@@ -5,6 +5,9 @@ import Criptomoneda from '../criptomoneda/Criptomoneda';
 function Formulario() {
 
     const [criptomonedas, getCriptomonedas] = useState([]);
+    const [monedaCotizar, getMonedaCotizar] = useState('');
+    const [criptoCotizar, getCotizar] = useState('');
+    const [error, gerError] = useState(false);
 
     useEffect(() => {
 
@@ -15,18 +18,21 @@ function Formulario() {
 
             //colocar respuesta en el state
             getCriptomonedas(resultado.data.Data);
-
-         
         }
 
         consultarApi();
     }, []);
 
+
+
     return(
         <form className="container formulario">
             <div className="row">
                 <label>Elige tu Moneda</label>
-                <select className="custom-select">
+                <select 
+                onChange={ e => getMonedaCotizar(e.target.value)}
+                className="custom-select"
+                >
                     <option value="">- Elige tu Moneda -</option>
                     <option value="USD">Dolar Estadounidense</option>
                     <option value="ARG">Peso Argentino</option>
@@ -37,7 +43,11 @@ function Formulario() {
 
             <div className="row">
                 <label>Elige tu Criptomoneda</label>
-                <select className="custom-select">
+                <select 
+                onChange={ e => getCotizar(e.target.value)}
+                className="custom-select"
+                >
+                    <option value="">- Elige tu Criptomoneda -</option>
                     {criptomonedas.map(criptomoneda => (
                         <Criptomoneda
                              key={criptomoneda.CoinInfo.Id}
