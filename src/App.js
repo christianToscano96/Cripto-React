8d994c3af56951873c57d42ff811f2ee315621f4
Christian Toscano <chrismarcadopago@gmail.com>
2019-08-02 03:13:12 -0300

    ajuste de imagen para modo responsive

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
       <div className="container pt-5 cripto-container">
         <div className="row ">
           <div className="col-sm-4">
-              <img src={img} className="img-fluid" alt="imagen de criptomonedas"/>
+              <img src={img} className="img-fluid img-cripto" alt="imagen de criptomonedas"/>
           </div>
           <div className="col-sm-8 formulario" >
             <h1 className="text-white text-center">Cotiza Criptomonedas al Instante</h1>
